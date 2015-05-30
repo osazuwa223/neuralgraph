339d9d136cff4bc9f6ff62553f3bb5124fc34c28
Robert Osazuwa Ness <robertness@gmail.com>
2015-05-29 22:58:29 -0400

    "optimization tests working"

--- tests/testthat/test-optimization.R
+++ tests/testthat/test-optimization.R
@@ -7,46 +7,41 @@ context("optimization")
 
 # Testing using Titanic3 data
 data(titanic3)
-titan <- dplyr::filter(titanic3, !is.na(age), !is.na(survived), !is.na(fare)) %>% #Not worrying about NA vals
-  {dplyr::mutate(., survived = as.numeric(survived))} %>%
-  {dplyr::select(., age, survived, fare)} %>%
+titan <- filter(titanic3, !is.na(age), !is.na(survived), !is.na(fare)) %>% #Not worrying about NA vals
+  mutate(survived = as.numeric(survived)) %>%
+  select(., age, survived, fare) %>%
   rescale_df %$% #Note, everything is rescaled to between 0 and 1
   df
 
 ###########################################################################################
 # Univariate no bias, no hidden layer case
 g <- mlp_graph("age", "survived") %>%
-  initializeGraph(input.table = select(titan, age), 
-                  output.table = select(titan, survived)) %>%
+  initializeGraph(select(titan, age, survived)) %>%
   {induced.subgraph(., V(.)[c("age", "survived")])} %>% # Having removed the bias, I need to reupdate 
   resetUpdateAttributes %>%
   updateSignals
 
-
 test_that("prediction from logistic function works as expected.", {
-  prediction <- unlist(V(getPrediction(g, V(g)["survived"], .5))[is.observed]$output.signal)
+  prediction <- unlist(V(getPrediction(g, V(g)["survived"], .5))["survived"]$output.signal)
   logistic(.5 * V(g)["age"]$output.signal[[1]]) %>% # calculation of prediction w/ weight of 5 via logistic function
-    identical(prediction) %>%# compared to algorithms generation of prediction
-    expect_true
+    expect_equal(prediction)# compared to algorithms generation of prediction  
 })
 
 ###########################################################################################
 # Multivariate with hidden layers
 g <- mlp_graph(c("age", "fare"), "survived", layers = c(3, 2)) %>%
-  initializeGraph(input.table = select(titan, age, fare), 
-                  output.table = select(titan, survived))
+  initializeGraph(select(titan, age, fare, survived))
 
 test_that("prediction from logistic function works as expected.", {
-  linear_combination <- V(g)[c("age", "fare", "int.3")]$output.signal %>% 
+  linear_combination <- V(g)[c("age", "fare", "bias_3")]$output.signal %>% 
     {do.call("cbind", .)} %>%
     `%*%`(matrix(c(-.5, -.5, -.5), ncol = 1)) %>% 
     as.numeric
   g <- resetUpdateAttributes(g)
   E(g)[to("H11")]$weight <- c(-.5, -.5, -.5)
-  updateSignals
+  g <- updateSignals(g)
   logistic(linear_combination) %>% # calculation of prediction w/ weight of 5 via logistic function
-    identical(unlist(V(g)["H11"]$output.signal)) %>%# compared to algorithms generation of prediction
-    expect_true
+    expect_equal(unlist(V(g)["H11"]$output.signal)) # compared to algorithms generation of prediction
 })
 
 ###########################################################################################
--- tests/testthat/test-templates.R
+++ tests/testthat/test-templates.R
@@ -1,6 +1,7 @@
 devtools::load_all("../../R/tools.R")
+#devtools::load_all("R/tools.R")
 context("templates for commmonly used signal graph models")
-
+library(dplyr)
 test_that("get_gate with all outputs works as expected", {
   gates <- c("AND", "OR", "NAND", "NOR", "XOR", "XNOR") 
   g <- get_gate(layers = c(2, 3))
@@ -19,16 +20,15 @@ test_that("get_gate with partial outputs works as expected", {
     expect_true  
 })
 
-test_that("get_gate produces the expected outputs on 2 input system", {
-  
+test_that("get_gate produces the expected outputs on 2 input system", {  
   # THis is what the output data frame should look like
   logic_gates <- expand.grid(list(I1 = c(0, 1), I2 = c(0, 1))) %>% 
-    {dplyr::mutate(., AND = (I1 * I2 == 1) * 1, 
+    mutate(AND = (I1 * I2 == 1) * 1, 
            OR = (I1 + I2 > 0) * 1 ,
            NAND = (!AND) * 1,
            NOR = (!OR) * 1,
            XOR = (I1 + I2 == 1) * 1, 
-           XNOR = (I1 == I2) * 1)}
+           XNOR = (I1 == I2) * 1)
   # So recreate this data frame from the function output and compare
   get_gate(layers = c(3, 3)) %>% 
     recover_design %>% #The outputs in the design should be the same as the logic_gates table
@@ -38,10 +38,9 @@ test_that("get_gate produces the expected outputs on 2 input system", {
 
 test_that("get_gate replicates a hand made version", {
   system <- expand.grid(list(I1 = c(0, 1), I2 = c(0, 1))) %>% 
-    {dplyr::mutate(., AND = (I1 * I2 == 1) * 1)}
+    mutate(AND = (I1 * I2 == 1) * 1)
   g1 <- get_gate("AND", c(3, 2))
   g2 <- mlp_graph(c("I1", "I2"), "AND", c(3, 2)) %>% #Use a 2 layer MLP
-    initializeGraph(input.table = select(system, I1, I2), 
-                    output.table = select(system, AND))
+    initializeGraph(select(system, I1, I2, AND))
   expect_equal(V(g1)$name, V(g2)$name)
 })
